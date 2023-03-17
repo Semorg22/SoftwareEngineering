e640c18532daf51064c11429ec89fd031bf6b2a6
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-03-17 16:17:51 -0500

    Update Ceil.java
    Atributos, Getters, Setters y Constructor

--- designPatterns/src/co/edu/poli/builder/model/Ceil.java
+++ designPatterns/src/co/edu/poli/builder/model/Ceil.java
@@ -2,4 +2,23 @@ package co.edu.poli.builder.model;
 
 public class Ceil {
 
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Ceil(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Ceil [tipo=" + tipo + "]";
+    }
+
 }
