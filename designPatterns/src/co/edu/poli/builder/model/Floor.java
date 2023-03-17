0823fe15ebf59e945f908463206fdb9c4e6d2cfe
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-03-17 16:18:10 -0500

    Update Floor.java
    Atributos, Getters, Setters y Constructor

--- designPatterns/src/co/edu/poli/builder/model/Floor.java
+++ designPatterns/src/co/edu/poli/builder/model/Floor.java
@@ -2,4 +2,33 @@ package co.edu.poli.builder.model;
 
 public class Floor {
 
+    private String tipo;
+    private int metros;
+
+    @Override
+    public String toString() {
+        return "Floor [tipo=" + tipo + ", metros=" + metros + "]";
+    }
+
+    public Floor(String tipo, int metros) {
+        this.tipo = tipo;
+        this.metros = metros;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getMetros() {
+        return metros;
+    }
+
+    public void setMetros(int metros) {
+        this.metros = metros;
+    }
+
 }
