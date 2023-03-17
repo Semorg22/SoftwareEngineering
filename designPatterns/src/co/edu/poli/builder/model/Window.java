adba82e183e08473366685268a194c9a9b402f99
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-03-17 16:18:27 -0500

    Update Window.java
    Atributos, Getters, Setters y Constructor

--- designPatterns/src/co/edu/poli/builder/model/Window.java
+++ designPatterns/src/co/edu/poli/builder/model/Window.java
@@ -2,4 +2,43 @@ package co.edu.poli.builder.model;
 
 public class Window {
 
+    private double ancho;
+    private double alto;
+    private double grosor;
+
+    public Window(double ancho, double alto, double grosor) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.grosor = grosor;
+    }
+
+    @Override
+    public String toString() {
+        return "Window [ancho=" + ancho + ", alto=" + alto + ", grosor=" + grosor + "]";
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getGrosor() {
+        return grosor;
+    }
+
+    public void setGrosor(double grosor) {
+        this.grosor = grosor;
+    }
+
 }
