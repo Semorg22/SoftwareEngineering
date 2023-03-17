b0312250fab1b782b29f47367d30bf5b221cd298
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-03-17 16:18:59 -0500

    Update Director.java
    Implementación de clases

--- designPatterns/src/co/edu/poli/builder/model/Director.java
+++ designPatterns/src/co/edu/poli/builder/model/Director.java
@@ -2,33 +2,45 @@ package co.edu.poli.builder.model;
 
 public class Director {
 
-	public House getHouse (String typeHouse) {
-		
+	public House getHouse(String typeHouse) {
+
 		House h;
-		
+
 		if (typeHouse.equals("h1")) {
+
+			Ceil techo = new Ceil("concreto");
+			Floor piso = new Floor("laminado", 70);
+			Door puerta = new Door(2.0, 60, "pino", 6);
+			Window ventana = new Window(60, 120, 3);
+			Pool piscina = new Pool(200, 400, 300);
+
 			h = new House.Builder()
 					.withType("apto")
-					.withCeil(null)
-					.withFloor(null)
-					.withDoor(null)
-					.withWindow(null)
-					.withPool(null)
+					.withCeil(techo)
+					.withFloor(piso)
+					.withDoor(puerta)
+					.withWindow(ventana)
+					.withPool(piscina)
 					.build();
-			
-			Garden g = new Garden();
+
+			Garden g = new Garden(5, 6);
 			h.setGarden(g);
-			
+
 		} else {
+
+			Ceil techo = new Ceil("concreto");
+			Window ventana = new Window(60, 120, 3);
+			Garden jardin = new Garden(6, 20);
+
 			h = new House.Builder()
 					.withType("house")
-					.withCeil(null)
-					.withWindow(null)
-					.withGarden(null)
+					.withCeil(techo)
+					.withWindow(ventana)
+					.withGarden(jardin)
 					.build();
 		}
-			
+
 		return h;
 	}
-	
+
 }
