d07a7553fc581c9728fa21239a3a625e1bed6952
Sebastian Moreno <56741293+Semorg22@users.noreply.github.com>
2023-03-17 16:18:15 -0500

    Update Pool.java
    Atributos, Getters, Setters y Constructor

--- designPatterns/src/co/edu/poli/builder/model/Pool.java
+++ designPatterns/src/co/edu/poli/builder/model/Pool.java
@@ -2,4 +2,43 @@ package co.edu.poli.builder.model;
 
 public class Pool {
 
+    private double ancho;
+    private double largo;
+    private double profund;
+
+    public Pool(double ancho, double largo, double profund) {
+        this.ancho = ancho;
+        this.largo = largo;
+        this.profund = profund;
+    }
+
+    @Override
+    public String toString() {
+        return "Pool [ancho=" + ancho + ", largo=" + largo + ", profund=" + profund + "]";
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public double getProfund() {
+        return profund;
+    }
+
+    public void setProfund(double profund) {
+        this.profund = profund;
+    }
+
 }
